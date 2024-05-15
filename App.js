7a3041851205f3231220a8973217422074436ddf
LAHSIVA <vishalvenkat2019@gmail.com>
2024-05-15 20:15:44 +0530

    feat : What heading will return js object

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
-const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello From React");
+const heading = React.createElement(
+    "h1",
+    {id:"heading",xyz:"abc"},
+    "Hello From React"
+);
+
+console.log(heading) //It will return React element or JAVASCRIPT object 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(heading); //It will take JS object has input it converts it into browser understandable and put it inside root.
